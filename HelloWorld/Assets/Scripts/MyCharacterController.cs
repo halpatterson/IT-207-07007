7d819d16e28e1fbf44e2a715e6b6418daae6d4b6
unknown <hal.patterson@ACC-GAME-19.lab.snhu.edu>
2014-11-24 13:00:49 -0500

    Commiting to project settings and assets the right way

---
+++ HelloWorld/Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCharacterController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.zero;
+		// Is Up pressed?
+		if (Input.GetKey (KeyCode.UpArrow) == true) {
+			// Move Character up by some amount
+			direction.y += 0.1f;
+		}
+		// Is Left Pressed?
+		if (Input.GetKey (KeyCode.LeftArrow) == true) {
+			// Move Character up by some amount
+			direction.x -= 0.1f;
+		}
+		// Is Right Pressed?
+		if (Input.GetKey (KeyCode.RightArrow) == true) {
+			// Move Character up by some amount
+			direction.x += 0.1f;
+		}
+		// Is Down Pressed?
+		if (Input.GetKey (KeyCode.DownArrow) == true) {
+						// Move Character down by some amount
+						direction.y -= 0.1f;
+				}
+		gameObject.transform.Translate (direction);
+	}
+}
